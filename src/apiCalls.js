70263da0eacda031e96edd9fe08d70114c906bc7
StevenTheBearded <stevenberg22@gmail.com>
2021-09-22 19:40:02 -0400

    Add apicall functoinality to retrieve data

---
+++ src/apiCalls.js
@@ -0,0 +1,10 @@
+export const fetchData = (city) => {
+
+  let apiKey = 'adb5990def9e15662c0b51eae2be289d'
+
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    .then(res => res.ok ? res.json() : console.log(res))
+    .then(data => data)
+}
+
+//const icon =  http://openweathermap.org/img/wn/${icon}@2x.png -- data.weather[0].icon
