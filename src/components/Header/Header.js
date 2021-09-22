0a284cde5f8346b0643cf6e80c35e2c1165c3e76
StevenTheBearded <stevenberg22@gmail.com>
2021-09-22 19:41:24 -0400

    Create state to store if displayed. Add prop to render search

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import SearchBar from '../SearchBar/Search';
+import './Header.css';
+
+const Header = ({ children }) => {
+  const [display, setDisplay] = useState(false)
+
+  return (
+    <header className='header'>
+      <h1>Weather App</h1>
+      {children}
+    </header>
+  )
+}
+
+export default Header;
