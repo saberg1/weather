aeddfd89c32aa34a15c2442040229b5daf5ce976
StevenTheBearded <stevenberg22@gmail.com>
2021-09-23 11:36:49 -0400

    Build search bar

--- src/components/SearchBar/Search.js
+++ src/components/SearchBar/Search.js
@@ -4,11 +4,34 @@ import { useState } from 'react';
 const SearchBar = () => {
   const [city, setCity] = useState('')
 
+  // var input = document.getElementById('autocomplete');
+  // var autocomplete = new google.maps.places.Autocomplete(input);
+
+  const handleClick = () => {
+    // console.log(city, ' :state inside search');
+    setCity('')
+  }
+
+  const handleChange = event => {
+    setCity(event.target.value);
+
+  };
+
   return(
     <div className='search'>
-      <input type='search' placeholder='Enter City'></input>
+      <input 
+        type='text' 
+        placeholder='Enter City'
+        value={city}
+        onChange={(event) => handleChange(event)}
+      ></input>
+      <button
+        className='button'
+        onClick={handleClick}>
+      Search
+      </button>
     </div>
   )
 }
 
-export default SearchBar
+export default SearchBar;
