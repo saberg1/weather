cecfd3c228aef11dedd07b8650fb84952845e9d5
StevenTheBearded <stevenberg22@gmail.com>
2021-09-23 22:26:30 -0400

    Add search by city functionality

--- src/components/SearchBar/Search.js
+++ src/components/SearchBar/Search.js
@@ -1,35 +1,55 @@
-import './Search.css';
 import { useState } from 'react';
+import PlacesAutocomplete, { geocodeByAddress, geocodeByPlaceId, getLatLng } from 'react-places-autocomplete';
+import './Search.css';
 
-const SearchBar = () => {
+const SearchBar = ({ retrieveCity }) => {
   const [city, setCity] = useState('')
+  const [coordinates, setCoordinates] = useState({
+    lat: null,
+    lng: null
+  })
 
-  // var input = document.getElementById('autocomplete');
-  // var autocomplete = new google.maps.places.Autocomplete(input);
-
-  const handleClick = () => {
-    // console.log(city, ' :state inside search');
-    setCity('')
+  const handleClick = (city) => {
+    // console.log(retrieveCity, ' :props on handleClick');
+    const formatedCity = city.split(' ')[0]
+    retrieveCity(formatedCity)
+    // console.log(value.split(' ')[0], ' :state on handleClick ');
   }
 
-  const handleChange = event => {
-    setCity(event.target.value);
-
-  };
+  const handleSelect = async (value) => {
+    // console.log('handleSelect invoked');
+    const result = await geocodeByAddress(value)
+    const latLng = await getLatLng(result[0])
+    setCoordinates(latLng)
+    setCity(value)
+    handleClick(value)
+  }
 
   return(
     <div className='search'>
-      <input 
-        type='text' 
-        placeholder='Enter City'
-        value={city}
-        onChange={(event) => handleChange(event)}
-      ></input>
-      <button
-        className='button'
-        onClick={handleClick}>
-      Search
-      </button>
+      <PlacesAutocomplete value={city} onChange={setCity} onSelect={handleSelect}>
+        { ( { getInputProps, suggestions, getSuggestionItemProps, loading } ) => (
+        <div>
+          <p>Latitude: {coordinates.lat}</p>
+          <p>Longitude: {coordinates.lng}</p>
+          <input {...getInputProps({ placeholder: 'City' } )} />
+
+          <div>
+            {loading ? <div>...loading</div> : null}
+            {suggestions.map(suggestion => {
+              const style = {
+                backgroundColor: suggestion.active ? '#000000' : '#fff',
+                color: suggestion.active ? '#fff' : '#000000'
+              }
+              return ( 
+                <div {...getSuggestionItemProps(suggestion, { style } ) }>
+                  {suggestion.description} 
+                </div>
+              );
+            })}
+          </div>
+        </div> ) }
+      </PlacesAutocomplete>
     </div>
   )
 }
