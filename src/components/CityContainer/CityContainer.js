651c493bb7cf8efa6c62d9a1e33c6698275bdcc1
StevenTheBearded <stevenberg22@gmail.com>
2021-09-24 20:54:09 -0400

    Add functionality to display cards

---
+++ src/components/CityContainer/CityContainer.js
@@ -0,0 +1,27 @@
+import CityCard from '../CityCard/CityCard';
+import './CityContainer.css';
+
+const CityContainer = ({ city }) => {
+
+    // const cityCards = city.map((ele) => {
+    //   return (
+    //     <CityCard 
+    //       key={ele.id}
+    //       id={ele.id}
+    //       name={ele.name}
+    //       hiTemp={ele.main.temp_max}
+    //       loTemp={ele.main.temp_min}
+    //       // icon={ele.weather[0].icon}
+    //       weather={ele.weather[0].description}
+    //     />
+    //   )
+    // })
+  return (
+    <section className='city-container'>
+      <p>hello</p>
+      {/* {cityCards} */}
+    </section>
+  )
+};
+
+export default CityContainer;
