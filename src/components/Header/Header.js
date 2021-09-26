eab38f65d5d8f6e287bb03728d8b3d21d79b34f4
StevenTheBearded <stevenberg22@gmail.com>
2021-09-26 19:03:22 -0400

    Delete comments

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,11 @@
-// import { useState } from 'react';
-// import SearchBar from '../SearchBar/Search';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = ({ renderPage, children }) => {
-  // const [display, setDisplay] = useState(false)
-  // console.log(children, ' :props in Header.js');
   return (
     <header className='header'>
       <Link to='/'> <h1>Weather App</h1> </Link>
        <Link to='/saved'> <button onClick={renderPage}> Show Favorites </button> </Link>
-       {/* <Link to='/'> <button> Home </button> </Link> */}
       <>
         {children} 
       </>
