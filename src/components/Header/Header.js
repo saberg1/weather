4d965d6615e20dc359d4fddf2401394fd7ff0fcc
StevenTheBearded <stevenberg22@gmail.com>
2021-09-26 15:43:07 -0400

    Functionality to view saved cards

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,19 @@
 // import { useState } from 'react';
 // import SearchBar from '../SearchBar/Search';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
-const Header = ({ renderPage }) => {
+const Header = ({ renderPage, children }) => {
   // const [display, setDisplay] = useState(false)
-
+  // console.log(children, ' :props in Header.js');
   return (
     <header className='header'>
-      <h1>Weather App</h1>
-      <button onClick={renderPage}> Show Favorites </button>
+      <Link to='/'> <h1>Weather App</h1> </Link>
+       <Link to='/saved'> <button onClick={renderPage}> Show Favorites </button> </Link>
+       {/* <Link to='/'> <button> Home </button> </Link> */}
+      <>
+        {children} 
+      </>
     </header>
   )
 }
