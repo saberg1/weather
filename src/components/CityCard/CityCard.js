ea884ae55318c61c71d321548c2114d0c9ef5c06
StevenTheBearded <stevenberg22@gmail.com>
2021-09-26 13:00:03 -0400

    Delelte comments

--- src/components/CityCard/CityCard.js
+++ src/components/CityCard/CityCard.js
@@ -1,21 +1,27 @@
 import './CityCard.css';
+import saved from '../../images/saved.png'
+import unsaved from '../../images/unsaved.png'
 
-const CityCard = ({ updateFavorite, cities, name, hiTemp, loTemp, icon, weather }) => {
+const CityCard = ({ fav, updateFavorite, cities, name, hiTemp, loTemp, icon, weather }) => {
 
   // const test = () => {
 
   //   console.log(cities, 'cities prop in CityCard');
   // }
-  
+  // const toggleImage = () => {
+  //   const image = fav ? '❤️' : '♡'
+  // }
   return(
     <article className='city-card'>
       <h1>{name}</h1>
       <h3>Hi:{hiTemp} | Lo:{loTemp}</h3>
       {/* <h3></h3> */}
-      <img src={icon}/>
+      {/* <img src={icon}/> */}
+
       <h3>{weather}</h3>
-      <button className='favorite' 
-        onClick={() => updateFavorite(cities)}>❤️</button>
+      <img className='favorite' 
+        src={fav ? saved : unsaved}
+        onClick={() => updateFavorite(cities)} />
     </article>
   )
 };
