1201138df1562ca649f722806349fbaf4ec97731
StevenTheBearded <stevenberg22@gmail.com>
2021-09-26 13:00:03 -0400

    Delelte comments

--- src/components/CityContainer/CityContainer.js
+++ src/components/CityContainer/CityContainer.js
@@ -5,56 +5,40 @@ import './CityContainer.css';
 const CityContainer = ({ cities, handleFavorite }) => {
   const [favorites, setFavorites] = useState([])
 
-//method to display between cities & favor
-
   const updateFavorite = (data) => {
-    // const test = data.isFavorited ? true : false 
-    // data.isFavorited = test
        if (data.isFavorited) {
-        // data.isFavorited = false
         remFav(data)
       } else {
-        // data.isFavorited = true
         addFav(data)
       }
-    // console.log('data.isFavorited: ', data.isFavorited);
-    //call next function to run depeneding on if favorted or unfavored
   }
   
   const remFav = (data) => {
     data.isFavorited = false
-    // console.log(data.isFavorited, ' :isFavorite inside remFav App.js');
     const removeFav = favorites.filter(city => city.id !== data.id)
     setFavorites(removeFav)
-    // console.log(data.isFavorited,` :${data.name} inside remFav CityContainer.js`);
     handleFavorite(data)
     remLocStor(data)
   }
 
   const addFav = (data) => {
     data.isFavorited = true
-    // console.log(data.isFavorited, ' :isFavorite inside addFav App.js');
     setFavorites([...favorites, data])
-    // console.log(data.isFavorited, ` :${data.name} inside addFav CityContainer.js`);
     handleFavorite(data)
     saveLocStor(data)
-    // console.log('addFav invoked');
-    // const updateData = data.isFavorited = true
-    // // const updateFav = {...data, updateData } 
-    // setFavorites({...data, updateData})
-    // // console.log(updateFav, ' :updateFav variable <<<>>>');
-    // console.log(data, ' :<<>><> data inside addFav CityContainer.js');
   }
 
   const remLocStor = (obj) => {
     localStorage.removeItem(obj.id)
-    // console.log(localStorage, ' :remLocStor');
   }
 
   const saveLocStor = (obj) => {
     localStorage.setItem(obj.id, JSON.stringify(obj))
-    // console.log(localStorage, ' :saveLocStor');
   }
+
+  useEffect(() => {
+  
+  }, [])
 
     const cityCards = cities.map( ele => {
       return (
@@ -64,6 +48,7 @@ const CityContainer = ({ cities, handleFavorite }) => {
           name={ele.name}
           hiTemp={ele.main.temp_max}
           loTemp={ele.main.temp_min}
+          fav={ele.isFavorited}
           // icon={ele.weather[0].icon}
           weather={ele.weather[0].description}
           cities={ele}
