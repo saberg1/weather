91fca7963936aabbfbac72e2e2a1fc5e97d99b91
StevenTheBearded <stevenberg22@gmail.com>
2021-09-27 11:05:53 -0400

    Render properties to card

--- src/components/SavedCity/SavedCity.js
+++ src/components/SavedCity/SavedCity.js
@@ -2,19 +2,19 @@ import './SavedCity.css';
 import saved from '../../images/saved.png'
 import unsaved from '../../images/unsaved.png'
 
-const SavedCity = ({ name, weather, handleFavorite, city, fav }) => {
+const SavedCity = ({ name,hiTemp, loTemp, fav, icon, weather, handleFavorite, city }) => {
 
   return(
     <article className='city-card'>
       <h1>{name}</h1>
-      {/* <h3>Hi:{hiTemp} | Lo:{loTemp}</h3> */}
+      <h3>Hi:{hiTemp} | Lo:{loTemp}</h3>
       {/* <h3></h3> */}
       {/* <img src={icon}/> */}
 
       <h3>{weather}</h3>
-      {/* <img className='favorite' 
+      <img className='favorite' 
         src={fav ? saved : unsaved}
-        onClick={() => updateFavorite(cities)} /> */}
+        onClick={() => handleFavorite(city)} />
         <button onClick={() => handleFavorite(city)}>favBtn</button>
     </article>
   )
