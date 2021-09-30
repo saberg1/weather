a94f6c4fa9b1444170f909dc497891438326a3ea
StevenTheBearded <stevenberg22@gmail.com>
2021-09-30 12:36:20 -0400

    refactor apicalls

--- cypress/integration/apiCalls_spec.js
+++ cypress/integration/apiCalls_spec.js
@@ -1,12 +1,14 @@
 describe('fetch call errors', () => {
   it('User should see error message if server responds 400 status code', () => {
     cy
-      .badFetch400()
+      .homePage().get('[type="text"]').type('detroit').should('have.value', 'detroit')
+      .interceptFetch(400).get('[type="text"]').type('{enter}').clear()
       .get('.error').contains('Sorry there is a problem with the servers. Please Try again later')
   })
   it('User should see error message if server responds 500 status code', () => {
     cy
-      .badFetch500()
+      .homePage().get('[type="text"]').type('detroit').should('have.value', 'detroit')
+      .interceptFetch(500).get('[type="text"]').type('{enter}').clear()
       .get('.error').contains('Sorry there is a problem with the servers. Please Try again later')
   })
 })
